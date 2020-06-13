3e169be27f9a4dcd4b4f3e3e7c256d5e78de677f
Daniel Orellana <daniel.orellana@outlook.com>
2020-06-12 23:47:24 -0500

    add pdf download, read, new response

--- app/script.py
+++ app/script.py
@@ -13,6 +13,7 @@ url = 'http://certificados.ministeriodegobierno.gob.ec/gestorcertificados/antece
 
 class selenium:
     wait = True
+    download_dir = ''
     
     def __init__(self):
         self.driver = self.iniciar()
@@ -22,19 +23,26 @@ class selenium:
     def iniciar(self): # Inicializar Webdriver
         try:
             gc = Options()
-            gc.add_argument('user-data-dir=sel')
+            gc.add_argument('user-data-dir=sel')        
             if os.name == 'nt':
                 print('Operative System> Windows')
-                return Chrome(options=gc, executable_path='./app/drivers/chromedriver.exe')
+                path = './app/drivers/chromedriver.exe' 
+                self.download_dir = 'C:\\Users\\danie\\Documents\\Proyectos\\vscode\\py\\selenium\\pyantecedentes\\app\\pdf'
             else:
                 print('Operative System> Unix')
                 gc.add_argument('headless')
                 gc.add_argument('no-sandbox')
-                return Chrome(options=gc, executable_path='./app/drivers/chromedriver')
+                path = './app/drivers/chromedriver'
+                self.download_dir = '/home/daniel/py/pyantecedentes/app/pdf'  # for linux/*nix, download_dir="/usr/Public"   
+            gc.add_experimental_option('prefs', {
+            "download.default_directory": self.download_dir, #Change default directory for downloads
+            "download.prompt_for_download": False, #To auto download the file
+            "download.directory_upgrade": False,
+            "plugins.always_open_pdf_externally": False #It will not show PDF directly in chrome
+            })
+            return Chrome(options=gc, executable_path=path)
         except Exception as e:
             print(e)
-        finally:
-            pass
 
     def button_click(self, button, timeout): # Buscar boton
         #print(button)
@@ -57,7 +65,8 @@ class selenium:
             return False
 
     def run(self, ci): # Main
-        re = {}
+        re = []
+        pdfurl = ''
         try:
             self.wait = False
             self.driver.get(url)
@@ -73,12 +82,22 @@ class selenium:
                 antecedentes = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "dvAntecedent1"))) # presence_of_element_located
                 endscript = datetime.datetime.now()
                 duration = endscript - startscript
-                re = {'CI': ci,'Name': str(name.text), 'Antecedentes': str(antecedentes.text), 'response': '{}'.format(str(duration))}
+                #re = {'CI': ci,'Name': str(name.text), 'Antecedentes': str(antecedentes.text), 'response': '{}'.format(str(duration))}
+                self.button_click('//*[@id="btnOpen"]/span', 5)
+                self.driver.switch_to.window(self.driver.window_handles[1])
+                pdfurl = self.driver.current_url
+                self.driver.close()
+                self.driver.switch_to.window(self.driver.window_handles[0])
+                print(pdfurl)
+                out_pdf = '{}.pdf'.format(ci)
+                os.system("wget -O {0} {1}".format(out_pdf, pdfurl))
+                re = True
             else:
-                re = {'error': 'ci not found'}
+                #re = {'error': 'ci not found'}
+                re = False
         except Exception as e:
             print(e)
-            return {'error': 'error'}
+            re = False
         finally:
             self.wait = True
-            return re
+            return [re, pdfurl]
--- app/views.py
+++ app/views.py
@@ -5,20 +5,45 @@ from app import app, q
 from app.script import selenium
 from flask import Flask, jsonify, request, render_template, abort
 
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+
 browser = selenium()
 
 def get_antecedentes(id):
     while True:
         if browser.wait == True:
-            re = browser.run(id)
+            status = browser.run(id)
             #job = q.enqueue_call(func=script.run, args=(ci,), timeout=-1)
             #id = job.get_id()
-            print(re)
-            return jsonify(re)
+            if status[0]:
+                re = pdf_reader(id, status[1])
+                return jsonify(re)
+            else:
+                abort(500, description="Uups")
+
+def pdf_reader(id, url):
+    pdf = "C:\\Users\\danie\\Documents\\Proyectos\\vscode\\py\\selenium\\pyantecedentes\\app\\pdf\\cert_ant_penales_{}.pdf".format(id)
+    pdffile = open(pdf, "rb")
+    pdfread = PdfFileReader(pdffile)
+    page = pdfread.getPage(0)
+    pageContent = page.extractText()
+    content = str(pageContent).splitlines()
+    re = {
+        'Fecha de Emision': content[0],
+        'Numero de Certificado': content[1],
+        'Tipo de Documento': content[2],
+        'No de Identificacion': content[3],
+        'Apellidos y Nombres': content[4],
+        'Registra Antecedentes': content[5],
+        'URL': url
+    }
+    return re
 
 @app.route("/api", strict_slashes=False)
 def api_doc():
-    return "Documentacion"
+    return pdf_reader()
+    #return "Documentacion"
 
 @app.route("/api/ci/<id>", strict_slashes=False) #
 def ci_antecedentes(id):
